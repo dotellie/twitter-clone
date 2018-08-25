62267bbb853f790819d6e89880a5ed2b5cbc23b8
Ellie <hi@ellie.moe>
2018-08-25 23:50:22 +0200

    Add basic routing

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const { db, initDB } = require('./db.js');
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello, World';
+const router = new Router({
+  prefix: '/api'
 });
+
+router.get('/', async ctx => {
+  ctx.body = 'Welcome to the Twitter clone API!';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 (async () => {
   try {
@@ -16,6 +24,7 @@ app.use(async ctx => {
     console.error(e);
     process.exit(1);
   }
+  app.context.db = db;
   app.listen(3000);
   console.log('Application listening on port 3000...');
 })();
