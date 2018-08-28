48d0b350fedbc3902e373bbd97476c7946161cf2
Ellie <hi@ellie.moe>
2018-08-28 22:26:47 +0200

    Add liking to tweets

--- frontend/src/components/tweet.vue
+++ frontend/src/components/tweet.vue
@@ -6,6 +6,15 @@
     <article>
       {{tweet['tweet_contents']}}
     </article>
+    <div class="tweet-footer">
+      <el-button
+        circle
+        :type="isLiked ? 'success' : ''"
+        :icon="isLiked ? 'el-icon-star-on' : 'el-icon-star-off'"
+        @click="toggleLike">
+      </el-button>
+      <span>{{tweet.likes.length}}</span>
+    </div>
   </el-card>
 </template>
 
@@ -22,6 +31,37 @@
       tweet: {
         default: {}
       }
+    },
+    computed: {
+      isLiked () {
+        const { id } = API.getUser();
+        return this.tweet.likes.includes(id);
+      }
+    },
+    methods: {
+      async toggleLike () {
+        const { id } = API.getUser();
+
+        if (this.isLiked) {
+          await API.post(`/tweet/${this.tweet['tweet_id']}/dislike`);
+          this.tweet.likes.splice(this.tweet.likes.indexOf(id));
+        } else {
+          await API.post(`/tweet/${this.tweet['tweet_id']}/like`);
+          this.tweet.likes.push(id);
+        }
+      }
     }
   }
 </script>
+
+<style>
+  .tweet-footer {
+    display: flex;
+    align-items: center;
+    margin-top: 1em;
+  }
+
+  .tweet-footer > *:not(:last-child) {
+    margin-right: 1em;
+  }
+</style>
